dffc8e63e2ad00c2548b4b2f5cb47e4a7202e6e1
Nick Yonge <nick@curiobot.com>
2024-10-15 12:26:34 -0700

    basic blink test src

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(5, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    digitalWrite(5, HIGH);
+    delay(500);
+    digitalWrite(5, LOW);
+    delay(500);
 }
