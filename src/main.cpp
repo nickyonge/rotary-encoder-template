941a447d56f5721cb2657787b5e10060075a92c4
Nick Yonge <nick@curiobot.com>
2024-10-15 16:50:16 -0700

    boolean LED mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
+// basic test script for using a rotary encoder with an ATtiny84
+
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 
 #define MODE_LED_BLINK
 #define MODE_LED_BOOLEAN
 
-#define PIN_LED_BLINK 5
 #define PIN_ENC_SWITCH 4
-
 #define PIN_ENC_INPUT_1 2 // INT0, PCINT10
 #define PIN_ENC_INPUT_2 3 // A7, PCINT7
+
+RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2);
+// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::TWO03);
+// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::FOUR0);
+// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::FOUR3);
+
+// ----------------------------- LED BLINK MODE SETUP
+#ifdef MODE_LED_BLINK
+#define PIN_LED_BLINK 5
 
 #define LED_BLINK_MIN 100
 #define LED_BLINK_MAX 2000
@@ -20,11 +29,26 @@
 int blinkTimer = 0;    // time value, constantly incrementing in ms
 int blinkLerp = 0;     // lerp value, 0-100, to map between LED_BLINK_MIN and LED_BLINK_MAX
 bool blinkLED = false; // is the LED currently on via blinking
+#endif                 // MODE_LED_BLINK
 
-RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2);
-// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::TWO03);
-// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::FOUR0);
-// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::FOUR3);
+// ----------------------------- LED BOOLEAN MODE SETUP
+#ifdef MODE_LED_BOOLEAN
+
+#define PIN_LED_BOOL_A 5
+#define PIN_LED_BOOL_B 6
+#define PIN_LED_BOOL_C 7
+#define PIN_LED_BOOL_D 8
+
+#define ENCODER_BOOLEAN_LOCK_INCREMENT // lock bool increment to +/- 1 per pulse
+
+int booleanValue = 0;
+
+// set boolean LED values based on an input int (value must be 0-15)
+void setBooleanLEDs(int value);
+// set boolean LED values directly
+void setBooleanLEDs(bool a, bool b, bool c, bool d);
+
+#endif // MODE_LED_BOOLEAN
 
 #pragma region PINMAPPING_CCW_DEFINITION
 // some basic warnings to ensure Arduino pinmapping is CCW
@@ -39,11 +63,22 @@ RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2);
 
 void setup()
 {
+
     // put your setup code here, to run once:
 
     pinMode(PIN_ENC_SWITCH, INPUT_PULLUP);
 
+#ifdef MODE_LED_BLINK
     pinMode(PIN_LED_BLINK, OUTPUT);
+#endif
+
+#ifdef MODE_LED_BOOLEAN
+    pinMode(PIN_LED_BOOL_A, OUTPUT);
+    pinMode(PIN_LED_BOOL_B, OUTPUT);
+    pinMode(PIN_LED_BOOL_C, OUTPUT);
+    pinMode(PIN_LED_BOOL_D, OUTPUT);
+    setBooleanLEDs(0);
+#endif
 }
 
 void loop()
@@ -61,6 +96,7 @@ void loop()
         int delta = newPos - pos;
 
 #ifdef MODE_LED_BLINK
+        // blink mode
         blinkLerp += delta * LED_BLINK_LERP_INCREMENT;
 #ifdef ENCODER_BLINK_WRAP
         // wrap around at 100
@@ -85,6 +121,31 @@ void loop()
 #endif
 #endif
 
+#ifdef MODE_LED_BOOLEAN
+        // boolean mode
+#ifdef ENCODER_BOOLEAN_LOCK_INCREMENT
+        if (delta > 0)
+        {
+            booleanValue++;
+        }
+        else if (delta < 0)
+        {
+            booleanValue--;
+        }
+#else
+        booleanValue += delta;
+#endif
+        while (booleanValue < 0)
+        {
+            booleanValue += 16;
+        }
+        while (booleanValue >= 16)
+        {
+            booleanValue -= 16;
+        }
+        setBooleanLEDs(booleanValue);
+#endif
+
         pos = newPos;
     }
 
@@ -101,6 +162,84 @@ void loop()
     digitalWrite(PIN_LED_BLINK, sw || blinkLED ? HIGH : LOW);
 #endif
 
+#ifdef MODE_LED_BOOLEAN
+    // boolean mode
+#endif
+
     // end loop
     delay(1);
 }
+
+#ifdef MODE_LED_BOOLEAN
+
+void setBooleanLEDs(int value)
+{
+    while (value < 0)
+    {
+        value += 16;
+    }
+    while (value >= 16)
+    {
+        value -= 16;
+    }
+    switch (value)
+    {
+    case 0:
+        setBooleanLEDs(false, false, false, false);
+        break;
+    case 1:
+        setBooleanLEDs(true, false, false, false);
+        break;
+    case 2:
+        setBooleanLEDs(false, true, false, false);
+        break;
+    case 3:
+        setBooleanLEDs(true, true, false, false);
+        break;
+    case 4:
+        setBooleanLEDs(false, false, true, false);
+        break;
+    case 5:
+        setBooleanLEDs(true, false, true, false);
+        break;
+    case 6:
+        setBooleanLEDs(false, true, true, false);
+        break;
+    case 7:
+        setBooleanLEDs(true, true, true, false);
+        break;
+    case 8:
+        setBooleanLEDs(false, false, false, true);
+        break;
+    case 9:
+        setBooleanLEDs(true, false, false, true);
+        break;
+    case 10:
+        setBooleanLEDs(false, true, false, true);
+        break;
+    case 11:
+        setBooleanLEDs(true, true, false, true);
+        break;
+    case 12:
+        setBooleanLEDs(false, false, true, true);
+        break;
+    case 13:
+        setBooleanLEDs(true, false, true, true);
+        break;
+    case 14:
+        setBooleanLEDs(false, true, true, true);
+        break;
+    case 15:
+        setBooleanLEDs(true, true, true, true);
+        break;
+    }
+}
+void setBooleanLEDs(bool a, bool b, bool c, bool d)
+{
+    digitalWrite(PIN_LED_BOOL_A, a ? HIGH : LOW);
+    digitalWrite(PIN_LED_BOOL_B, b ? HIGH : LOW);
+    digitalWrite(PIN_LED_BOOL_C, c ? HIGH : LOW);
+    digitalWrite(PIN_LED_BOOL_D, d ? HIGH : LOW);
+}
+
+#endif
