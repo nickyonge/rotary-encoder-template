00e7a49454ae0775852fcfbe84a6ef9f647757fd
Nick Yonge <nick@curiobot.com>
2024-10-15 15:46:26 -0700

    moved blink into its own logic def

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 
-#define PIN_LED 5
-#define PIN_SWITCH 4
+#define MODE_LED_BLINK
+#define MODE_LED_BOOLEAN
 
-#define PIN_IN1 2 // INT0, PCINT10
-#define PIN_IN2 3 // A7, PCINT7
+#define PIN_LED_BLINK 5
+#define PIN_ENC_SWITCH 4
 
-#define BLINK_MIN 100
-#define BLINK_MAX 2000
+#define PIN_ENC_INPUT_1 2 // INT0, PCINT10
+#define PIN_ENC_INPUT_2 3 // A7, PCINT7
 
-#define ENCODER_BLINK_WRAP // comment out to lock lerp at 0-100, uncomment to wrap 
+#define LED_BLINK_MIN 100
+#define LED_BLINK_MAX 2000
 
-#define BLINK_INCREMENT 1
+#define ENCODER_BLINK_WRAP // comment out to lock lerp at 0-100, uncomment to wrap
+
+#define LED_BLINK_LERP_INCREMENT 1
 
 int blinkTimer = 0;    // time value, constantly incrementing in ms
-int blinkLerp = 0;     // lerp value, 0-100, to map between BLINK_MIN and BLINK_MAX
+int blinkLerp = 0;     // lerp value, 0-100, to map between LED_BLINK_MIN and LED_BLINK_MAX
 bool blinkLED = false; // is the LED currently on via blinking
 
-RotaryEncoder encoder(PIN_IN1, PIN_IN2);
-// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
-// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR0);
-// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2);
+// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::TWO03);
+// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::FOUR0);
+// RotaryEncoder encoder(PIN_ENC_INPUT_1, PIN_ENC_INPUT_2, RotaryEncoder::LatchMode::FOUR3);
 
 #pragma region PINMAPPING_CCW_DEFINITION
 // some basic warnings to ensure Arduino pinmapping is CCW
@@ -38,53 +41,66 @@ void setup()
 {
     // put your setup code here, to run once:
 
-    pinMode(PIN_SWITCH, INPUT_PULLUP);
+    pinMode(PIN_ENC_SWITCH, INPUT_PULLUP);
 
-    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_LED_BLINK, OUTPUT);
 }
 
 void loop()
 {
 
     // get switch input
-    static bool sw = !digitalRead(PIN_SWITCH); // if sw true, force LED on
+    static bool sw = !digitalRead(PIN_ENC_SWITCH); // if sw true, force LED on
 
     // get encoder input
     static int pos = 0;
     encoder.tick();
     int newPos = encoder.getPosition();
-    if (pos != newPos) {
+    if (pos != newPos)
+    {
         int delta = newPos - pos;
-        blinkLerp += delta * BLINK_INCREMENT;
-        #ifdef ENCODER_BLINK_WRAP
-        //wrap around at 100 
-        while (blinkLerp < 0) {
+
+#ifdef MODE_LED_BLINK
+        blinkLerp += delta * LED_BLINK_LERP_INCREMENT;
+#ifdef ENCODER_BLINK_WRAP
+        // wrap around at 100
+        while (blinkLerp < 0)
+        {
             blinkLerp += 100;
         }
-        while (blinkLerp > 100) {
+        while (blinkLerp > 100)
+        {
             blinkLerp -= 100;
         }
-        #else
-        // cap to 100 
-        if (blinkLerp < 0) {
+#else
+        // cap to 100
+        if (blinkLerp < 0)
+        {
             blinkLerp = 0;
-        } else if (blinkLerp > 100) {
+        }
+        else if (blinkLerp > 100)
+        {
             blinkLerp = 100;
         }
-        #endif
+#endif
+#endif
+
         pos = newPos;
     }
 
-    // update blink timer 
+#ifdef MODE_LED_BLINK
+    // update blink timer
     blinkTimer++;
-    if (blinkTimer > map(blinkLerp, 0, 100, BLINK_MIN, BLINK_MAX))
+    if (blinkTimer > map(blinkLerp, 0, 100, LED_BLINK_MIN, LED_BLINK_MAX))
     {
         blinkLED = !blinkLED;
         blinkTimer = 0;
     }
 
     // put your main code here, to run repeatedly:
-    digitalWrite(PIN_LED, sw || blinkLED ? HIGH : LOW);
+    digitalWrite(PIN_LED_BLINK, sw || blinkLED ? HIGH : LOW);
+#endif
 
+    // end loop
     delay(1);
 }
