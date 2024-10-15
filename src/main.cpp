91584ce78b4cfb9bf7a7b385244d9d70067c3cdd
Nick Yonge <nick@curiobot.com>
2024-10-15 12:32:48 -0700

    reading rotary encoder switch (basic)

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <Arduino.h>
 
+#define PIN_LED 5
+#define PIN_SWITCH 6
+
 void setup()
 {
     // put your setup code here, to run once:
-    pinMode(5, OUTPUT);
+
+    pinMode(PIN_SWITCH, INPUT_PULLUP);
+
+    pinMode(PIN_LED, OUTPUT);
 }
 
 void loop()
 {
+
+    bool sw = digitalRead(PIN_SWITCH);
+
     // put your main code here, to run repeatedly:
-    digitalWrite(5, HIGH);
-    delay(500);
-    digitalWrite(5, LOW);
-    delay(500);
+    digitalWrite(PIN_LED, sw ? HIGH : LOW);
+    delay(50);
 }
