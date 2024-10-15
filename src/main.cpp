d81a78e100d4436d91bd1cb8bbbab397f4bf31cf
Nick Yonge <nick@curiobot.com>
2024-10-15 14:40:10 -0700

    basic timed blinking with min/max lerp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 #define PIN_LED 5
 #define PIN_SWITCH 4
 
+#define BLINK_MIN 100
+#define BLINK_MAX 2000
+
+#define BLINK_INCREMENT 5
+
+int blinkTimer = 0;    // time value, constantly incrementing in ms
+int blinkLerp = 0;     // lerp value, 0-100, to map between BLINK_MIN and BLINK_MAX
+bool blinkLED = false; // is the LED currently on via blinking
+
 #pragma region PINMAPPING_CCW_DEFINITION
 // some basic warnings to ensure Arduino pinmapping is CCW
 #ifdef PINMAPPING_CW
@@ -26,9 +35,17 @@ void setup()
 void loop()
 {
 
-    bool sw = !digitalRead(PIN_SWITCH);
+    bool sw = !digitalRead(PIN_SWITCH); // if sw true, force LED on
+
+    blinkTimer++;
+    if (blinkTimer > map(blinkLerp, 0, 100, BLINK_MIN, BLINK_MAX))
+    {
+        blinkLED = !blinkLED;
+        blinkTimer = 0;
+    }
 
     // put your main code here, to run repeatedly:
-    digitalWrite(PIN_LED, sw ? HIGH : LOW);
-    delay(50);
+    digitalWrite(PIN_LED, sw || blinkLED ? HIGH : LOW);
+
+    delay(1);
 }
