a87f5fad0eddf355c32c6edd0c534034c91a1deb
Nick Yonge <nick@curiobot.com>
2024-10-15 13:12:25 -0700

    error checks to ensure correct ccw arduino pinmapping

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 #define PIN_LED 5
 #define PIN_SWITCH 6
 
+#pragma region PINMAPPING_CCW_DEFINITION
+// some basic warnings to ensure Arduino pinmapping is CCW
+#ifdef PINMAPPING_CW
+#error "Code written for CCW pin mapping!"
+#else
+#ifndef PINMAPPING_CCW
+#error "Code written for CCW pin mapping, but PINMAPPING_CCW is undefined!"
+#endif
+#endif
+#pragma endregion PINMAPPING_CCW_DEFINITION
+
 void setup()
 {
     // put your setup code here, to run once:
